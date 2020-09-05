567083f399bb36284911fbfd248be3519259ecb2
Jeannie Kim <sj.jeannie.kim@gmail.com>
2020-09-05 12:55:47 -0500

    showing habits on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,8 @@ import FetchData from './fetch.js';
 const HelloWorld = () => {
   return (
     <div>
-      <FetchData />
       <h1>Hello world</h1>
+      <FetchData />
       <h2>Jeannie was here</h2>
     </div>
   );
